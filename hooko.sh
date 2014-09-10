153dae4afb06586bba4fdd713dac8b1f03454853
Anton Borsch <anton.borsch@anahoret.com>
2014-09-10 11:52:59 +0300

    Add .cssconf.json config to root of app. Add colours for messages.

--- hooko.sh
+++ hooko.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+ESC_SEQ="\x1b["
+COL_CYAN=$ESC_SEQ"36;01m"
+COL_RESET=$ESC_SEQ"39;49;00m"
+
 exists() {
   if command -v $1 &>/dev/null
   then
@@ -9,18 +13,24 @@ exists() {
   fi
 }
 
-file='https://raw.githubusercontent.com/Oversan/Hooko/master/pre-commit'
-successMsg='Pre-commit hook is added to .git/hooks of this project'
+hook='https://raw.githubusercontent.com/Oversan/Hooko/master/pre-commit'
+config='https://raw.githubusercontent.com/Oversan/Hooko/master/.csscomb.json'
+successHookMsg=$COL_CYAN'Pre-commit hook is added to .git/hooks of this project'$COL_RESET
+successConfigMsg=$COL_CYAN'Config .csscomb.json is added.'$COL_RESET
 
 if exists wget; then
-  wget $file -P .git/hooks
-  echo $successMsg
+  wget $hook -P .git/hooks
+  echo -e $successHookMsg
+  wget $config
+  echo -e $successConfigMsg
 else
   if exists curl; then
-    curl $file > .git/hooks/pre-commit
-    echo $successMsg
+    curl $hook > .git/hooks/pre-commit
+    echo -e $successHookMsg
+    curl -O $config
+    echo -e $successConfigMsg
   else
-  	echo "You have no installed curl or wget!!!"
+    echo $COL_CYAN"You have no installed curl or wget!!!"$COL_RESET
   fi
 fi
 
