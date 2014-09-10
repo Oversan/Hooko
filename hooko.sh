9c300a9dcfec30441443e8fcfc6596cf7b3a3b10
Anton Borsch <anton.borsch@anahoret.com>
2014-09-10 11:06:12 +0300

    Remove duplication. Replace message to variable.

--- hooko.sh
+++ hooko.sh
@@ -10,13 +10,17 @@ exists() {
 }
 
 file='https://raw.githubusercontent.com/Oversan/Hooko/master/pre-commit'
+successMsg='Pre-commit hook is added to .git/hooks of this project'
 
 if exists wget; then
   wget $file -P .git/hooks
+  echo $successMsg
 else
   if exists curl; then
     curl $file > .git/hooks/pre-commit
+    echo $successMsg
+  else
+  	echo "You have no installed curl or wget!!!"
   fi
 fi
 
-echo "pre-commit hook is added to .git/hooks of this project"
